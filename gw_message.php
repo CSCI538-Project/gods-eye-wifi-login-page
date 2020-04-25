900401816ebff512a76f199b8f3fd7a7ccf59aab
liwenbin0320 <liwenbin@liwenbin.com>
2020-04-24 22:44:01 -0700

    replace

--- gw_message.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: rain
- * Date: 2/29/20
- * Time: 16:24
- */
-
-echo $_GET['message'];
--- ping/index.php
+++
@@ -1,8 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: rain
- * Date: 2/29/20
- * Time: 16:20
- */
-echo "Pong";
